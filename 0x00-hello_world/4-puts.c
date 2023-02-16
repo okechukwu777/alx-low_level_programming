7a19f72601f41bc1a861a8c7a56391c13725bd3e
okechukwu777 <nwaewo@gmail.com>
2023-02-16 03:04:34 -0800

    prints exactly "Programming is like building a multilingual puzzle, followed by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - this is a c program that prints a line with put function
+ * Return: 0 (success)
+ */
+{
+	puts ("\-Programming is like building a multilingual puzzle");
+return (0);
+}
