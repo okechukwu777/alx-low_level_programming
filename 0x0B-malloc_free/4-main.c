535d077f514505ae03351a3c73b2740dab9d495f
okechukwu777 <nwaewo@gmail.com>
2023-03-14 08:32:58 -0700

    a function that concatenates all the arguments of your program.

--- 0x0B-malloc_free/4-main.c
+++ 0x0B-malloc_free/4-main.c
@@ -30,7 +30,7 @@ void print_grid(int **grid, int width, int height)
 }
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
